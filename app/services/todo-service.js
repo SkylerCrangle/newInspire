c2b0461d94b61fd653677b58a7e6bdc87ef84d1f
SkylerCrangle <smcrangle@gmail.com>
2020-02-15 16:39:55 -0700

    checking true

--- app/services/todo-service.js
+++ app/services/todo-service.js
@@ -51,7 +51,7 @@ class TodoService {
         let todo = store.State.todo.find(t => t._id == todoId);
         todo.completed = true
         console.log(todo)
-        store.commit("todo", todo)
+        store.commit("todo", store.State.todo)
       })
       .catch(error => {
         console.error(error);
